65f10eaf41dcdbfecd1a37da2d272e8703d1bea3
luzybry94 <luzybry94@gmail.com>
2020-11-19 14:05:46 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,11 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    set_student
+    @student.activate
+  end
+
   private
 
     def set_student
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Luzy", last_name: "Bryski")
+Student.create(first_name: "Chaim", last_name: "Hakohen")
