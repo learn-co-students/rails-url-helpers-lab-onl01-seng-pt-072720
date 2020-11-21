5f30461d0e67d8d360974d1107dcaef2210d86d8
fentyhall <fentymhall@gmail.com>
2020-11-21 12:41:27 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,18 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    set_student
+    if @student.active == false
+      @student.active = true
+    else 
+      @student.active = false
+    end
+    @student.save
+
+    redirect_to "/students/#{@student.id}"
+  end 
+
   private
 
     def set_student
